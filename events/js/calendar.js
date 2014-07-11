7b9c5f03c159e356e40b89a741e7cbc3be373e58
Eric Jameson <e-jameson@bethel.edu>
2014-07-11 00:52:14 -0500

    test

--- events/js/calendar.js
+++ events/js/calendar.js
@@ -259,7 +259,7 @@
             //using query params instead of hash
             h = window.location.search.replace(/^\#/, '?') || '?';
         }
-        loc = '//staging.bethel.edu/events/calendar/code/calendar_rest' + h;
+        loc = '/events/calendar/code/calendar_rest' + h;
         changeCalendarLocation(loc);
     }
 
