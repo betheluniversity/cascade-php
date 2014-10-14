c7a53a97fc4c97a7a7a669efddbd599caf9d376f
Eric Jameson <e-jameson@bethel.edu>
2014-10-14 09:52:35 -0500

    use total dollar amount

--- roar-day/total-gifts.php
+++ roar-day/total-gifts.php
@@ -6,7 +6,7 @@
  * Time: 12:58 PM
  */
 
-    $gifts = json_decode(file_get_contents("http://wsapi.bethel.edu/roar/total_gifts_given"));
+    $gifts = json_decode(file_get_contents("http://wsapi.bethel.edu/roar/total-gifts"));
     //print_r($gifts);
     $total = $gifts->{'result'}[0][0];
-    echo "$total";
+    echo "$$total";
