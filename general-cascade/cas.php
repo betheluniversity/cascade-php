719d4061e890be4d405fbd67a6e4473fefe069c2
Eric Jameson <e-jameson@bethel.edu>
2014-10-17 08:33:48 -0500

    remove ticket

--- general-cascade/cas.php
+++ general-cascade/cas.php
@@ -16,13 +16,26 @@ phpCAS::client(CAS_VERSION_3_0, $cas_host, $cas_port, $cas_context);
 phpCAS::setServerServiceValidateURL("https://auth.bethel.edu/cas/serviceValidate");
 
 if($staging){
-    $prefix = 'https://staging.bethel.edu';
+    $final_url = 'https://staging.bethel.edu';
 }else{
-    $prefix = 'https://www.bethel.edu';
+    $final_url = 'https://www.bethel.edu';
 }
+
+$request_uri	= explode('?', $_SERVER['REQUEST_URI'], 2);
+$final_url		.= $request_uri[0];
+
+if (isset($request_uri[1]) && $request_uri[1]) {
+    $query_string= _removeParameterFromQueryString('ticket', $request_uri[1]);
+    // If the query string still has anything left,
+    // append it to the final URI
+    if ($query_string !== '') {
+        $final_url	.= "?$query_string";
+    }
+}
+
 phpCAS::setServerLoginURL('https://auth.bethel.edu/cas/login?service=' . $prefix . $_SERVER['REQUEST_URI']);
-echo 'using ' . $prefix . $_SERVER['REQUEST_URI'];
-phpCAS::setFixedServiceURL($prefix . $_SERVER['REQUEST_URI']);
+echo 'using ' . $final_url;
+phpCAS::setFixedServiceURL($final_url);
 //phpCAS::setNoCasServerValidation();
 phpCAS::setCasServerCACert("/etc/pki/tls/certs/gd_bundle.crt");
 
@@ -39,5 +52,15 @@ if($require_auth == "Yes"){
     }else{
         $remote_user = null;
     }
+}
+
+
+function _removeParameterFromQueryString($parameterName, $queryString)
+{
+    $parameterName	= preg_quote($parameterName);
+    return preg_replace(
+        "/&$parameterName(=[^&]*)?|^$parameterName(=[^&]*)?&?/",
+        '', $queryString
+    );
 }
 
