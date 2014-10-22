0dbdeb47fa1b99830d37ee7b4ceaf41c8c823e9b
Eric Jameson <e-jameson@bethel.edu>
2014-10-22 15:18:58 -0500

    less time

--- general-cascade/metadata-check.php
+++ general-cascade/metadata-check.php
@@ -22,5 +22,5 @@ if ($cms_url) {
 if ($require_auth == "Yes" || $check_auth == "Yes"){
     include_once 'cas.php';
 }else{
-    header("Cache-Control: public, must-revalidate, max-age=2592000");
+    header("Cache-Control: public, must-revalidate, max-age=86400");
 }
