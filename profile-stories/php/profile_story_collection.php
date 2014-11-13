77f4f5ed3fb3c1fb616113182ffbf0f6029a556a
johjacb <johjacb@bethel.edu>
2014-11-13 08:48:42 -0600

    Update profile_story_collection.php
    tweaking carousel html

--- profile-stories/php/profile_story_collection.php
+++ profile-stories/php/profile_story_collection.php
@@ -104,16 +104,18 @@
             $teaser = $viewerTeaser;
         }
         $quote = $ds->{'quote'};
-        $html = '<a class="carousel-item" href="http://bethel.edu'.$xml->path.'">';
+        $html = '<div class="slick-item">';
+        $html .= '<a href="http://bethel.edu'.$xml->path.'">';
             //$html .= render_image($imagePath, $teaser, "feature__img", "100%", $destinationName); Old version, feature__img didn't work
-            $html .= render_image($imagePath, $teaser, "delayed-image-load", "100%", $destinationName);
-
+            //$html .= render_image($imagePath, $teaser, "delayed-image-load", "100%", $destinationName);
+            $html .= '<img data-lazy="$imagePath"/>';
             $html .= '<figure class="feature__figure">';
             $html .= '<blockquote class="feature__blockquote">'.$quote.'</blockquote>';
             $html .= '<figcaption class="feature__figcaption">'.$teaser.'</figcaption>';
 
             $html .= '</figure>';
         $html .= '</a>';
+        $html .= '</div>';
 
         return $html;
     }
