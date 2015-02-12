2b5ab6f77de51272be65baf183b4b9417a509ddf
Eric Jameson <e-jameson@bethel.edu>
2015-02-12 14:11:13 -0600

    add srcset function

--- general-cascade/metadata-check.php
+++ general-cascade/metadata-check.php
@@ -25,3 +25,17 @@ if ($require_auth == "Yes" || $check_auth == "Yes"){
 }else{
     header("Cache-Control: public, must-revalidate, max-age=86400");
 }
+
+function srcset($end_path){
+    $rand = rand(1,4);
+    echo "<img srcset='
+                //cdn$rand.bethel.edu/resize/unsafe/1400x0/smart/$end_path 1400w,
+                //cdn$rand.bethel.edu/resize/unsafe/1200x0/smart/$end_path 1200w,
+                //cdn$rand.bethel.edu/resize/unsafe/1000x0/smart/$end_path 1000w,
+                //cdn$rand.bethel.edu/resize/unsafe/800x0/smart/$end_path 800w,
+                //cdn$rand.bethel.edu/resize/unsafe/600x0/smart/$end_path 600w,
+                //cdn$rand.bethel.edu/resize/unsafe/400x0/smart/$end_path 400w,
+                //cdn$rand.bethel.edu/resize/unsafe/200x0/smart/$end_path 200w'
+                src='//cdn$rand.bethel.edu/resize/unsafe/320x0/smart/$end_path'></img>";
+
+}
