d5f5df750804b6b58f9254da28554a6a69ef07cf
Eric Jameson <e-jameson@bethel.edu>
2015-05-12 14:37:48 -0500

    how to apply function

--- banner-blocks/capsgs_checklist.php
+++ banner-blocks/capsgs_checklist.php
@@ -7,21 +7,25 @@
  */
 
 
-$twig = makeTwigEnviron('/code/banner-blocks/twig');
-
-
-if($select_option == 4){
-    return;
-}else{
-    $call_program_code = str_replace("%", "%25", $program_code);
-    $url = "http://wsapi.bethel.edu/capsgs-checklist/$call_program_code/$select_option";
-    echo "<!-- $url -->";
-    $results = json_decode(file_get_contents($url));
-    $results = explode("\n", $results->data);
-    if($results[0]){
-        echo $twig->render('capsgs_checklist.html', array(
-            'results' => $results));
+function howToApply($code, $option){
+    $twig = makeTwigEnviron('/code/banner-blocks/twig');
+    if($select_option == 4){
+        return;
+    }else{
+        $call_program_code = str_replace("%", "%25", $code);
+        $url = "http://wsapi.bethel.edu/capsgs-checklist/$call_program_code/$option";
+        echo "<!-- $url -->";
+        $results = json_decode(file_get_contents($url));
+        $results = explode("\n", $results->data);
+        if($results[0]){
+            echo $twig->render('capsgs_checklist.html', array(
+                'results' => $results));
+        }
     }
 }
+
+
+
+
 
 
