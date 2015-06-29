34f6cdf9e2639e16be271922131ea865cb6477e6
Eric Jameson <e-jameson@bethel.edu>
2015-06-29 16:00:04 -0500

    add file to send request to wsapi / TOD

---
+++ tod/tod_request.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ejc84332
+ * Date: 6/29/15
+ * Time: 2:01 PM
+ */
+
+$staging = strstr(getcwd(), "staging/public");
+$soda = strstr(getcwd(), "soda");
+$require_auth = "Yes";
+
+include_once $_SERVER["DOCUMENT_ROOT"] . "/code/general-cascade/cas.php";
+
+$username = $remote_user;
+header("Location: http://wsapi.xp.bethel.edu/tod/$username");
