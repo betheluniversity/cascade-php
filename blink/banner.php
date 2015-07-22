4eac3f42e77b2bc4e80a9f04c2f2cc0482abeeac
c-schwarze <ces55739@bethel.edu>
2015-07-22 10:33:54 -0500

    Updated log file for blink/banner

--- blink/banner.php
+++ blink/banner.php
@@ -28,8 +28,9 @@ if($staging){
 }
 
 // logging
-$current_time = date("m/d/Y h:i:s");
-error_log("$current_time 1) Blink channel to Banner url: " + $_GET['url']);
-error_log("$current_time 2) Submitted url: $url");
+ini_set("log_errors", 1);
+ini_set("error_log", "php-error.log");
+error_log("1) Original blink/banner url after going through varnish: " + $_GET['url']);
+error_log("2) Submitted url after going through php script: $url");
 
 header( 'Location: ' . $url ) ;
