bf4dd13113521b4f235b95b50417263afb98dd30
calebV <cav28255@bethel.edu>
2015-11-12 13:31:36 -0600

    updating faculty carousel for new job titles

--- collections/php/faculty.php
+++ collections/php/faculty.php
@@ -15,14 +15,20 @@ function create_faculty_carousel($categories){
 
     $carousel_items = "";
     foreach($bios as $bio){
+
         $ds = $bio->{'system-data-structure'};
         $first = $ds->first;
         $last = $ds->last;
-        $title = $ds->{'job-title'};
         $path = $bio->path;
         $image = "https://www.bethel.edu" . $ds->image->path[0];
-        $html = create_twig_html($image, $first, $last, $title, $path);
-
+        $title = $ds->{'job-title'};
+        $titles = $ds->{'job-titles'};
+        if($titles[0]->{'school'} != "") {
+            $jobsAsSting = create_new_job_titles($titles);
+            $html = create_twig_html($image, $first, $last, $jobsAsSting, $path);
+        }else {
+            $html = create_twig_html($image, $first, $last, $title, $path);
+        }
         $carousel_items .= carousel_item($html, "", null, false);
     }
     carousel_create("js-flickity  carousel--employee", $carousel_items);
@@ -53,6 +59,8 @@ function find_matching_bios($xml, $categories){
     return $return_bios;
 }
 
+
+
 function create_twig_html($image, $first, $last, $title, $path){
 
     $twig = makeTwigEnviron('/code/collections/twig');
@@ -66,4 +74,75 @@ function create_twig_html($image, $first, $last, $title, $path){
         'thumbURL'  => $thumbURL
     ));
     return $html;
+}
+
+function create_new_job_titles($titles){
+    $job_map = array();
+    $job_title_array = array();
+    $school_array = array();
+    $program_array = array();
+
+    foreach($titles as $title){
+        array_push($school_array, $title->{'school'});
+
+        if($title->{'department'}){
+            array_push($program_array, $title->{'department'});
+        }elseif($title->{'adult-undergrad-program'}){
+            array_push($program_array, $title->{'adult-undergrad-program'});
+        }elseif($title->{'graduate-program'}){
+            array_push($program_array, $title->{'graduate-program'});
+        }elseif($title->{'seminary-program'}){
+            array_push($program_array, $title->{'seminary-program'});
+        }else{
+            array_push($program_array, '');
+        }
+
+        if($title->{'department-chair'} == 'Yes'){
+            array_push($job_title_array, 'Department Chair');
+        }elseif($title->{'program-director'} == 'Yes'){
+            array_push($job_title_array, 'Program Director');
+        }elseif($title->{'lead-faculty'} == 'Lead Faculty' || $title->{'lead-faculty'} == 'Program Director'){
+            array_push($job_title_array, $title->{'lead-faculty'});
+        }elseif($title->{'job_title'}){
+            array_push($job_title_array, $title->{'job_title'});
+        }else{
+            array_push($job_title_array, " ");
+        }
+    }
+    for($i = 0; $i < sizeof($school_array); $i++) {
+        $currentSchool = $school_array[$i];
+        $currentProgram = $program_array[$i];
+        $currentJob = $job_title_array[$i];
+        if(!array_key_exists($currentSchool, $job_map)){
+            $job_map["$currentSchool"] = array();
+        }
+        if(!array_key_exists($currentSchool, $job_map["$currentSchool"])){
+            $job_map["$currentSchool"]["$currentProgram"] = array();
+        }
+        if(!array_key_exists($currentSchool, $job_map["$currentSchool"]["$currentProgram"])){
+            array_push($job_map["$currentSchool"]["$currentProgram"], $currentJob);
+        }
+
+    }
+    $jobsAsString = "";
+    foreach($job_map as $key => $value){
+        foreach($value as $key2 => $value2) {
+            $size = sizeof($value2);
+            for($j = 0; $j < $size; $j++) {
+                if($j == ($size-1) && $size > 1){
+                    $jobsAsString = $jobsAsString . " and ";
+                }
+                $jobsAsString = $jobsAsString . "$value2[$j]";
+                if($j < ($size-1) && $size > 2){
+                    $jobsAsString = $jobsAsString . ", ";
+                }
+            }
+            if($key2 != "None") {
+                $jobsAsString = $jobsAsString . " in $key2 ";
+            }
+        }
+        
+    }
+    return $jobsAsString;
+
 }
