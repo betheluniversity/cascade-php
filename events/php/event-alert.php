f339069e4cfb05dbc926de27eede591c4929faa9
c-schwarze <ces55739@bethel.edu>
2016-04-27 11:17:04 -0500

    Event Alert

---
+++ events/php/event-alert.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ces55739
+ * Date: 4/27/16
+ * Time: 10:17 AM
+ */
+
+function create_event_alert($schools, $offCampusLocation, $firstDate){
+    // convert cascade timestamp to a normal timestamp, and then to a date
+    $firstDate = Date((int)$firstDate/1000);
+    $today = time();
+    
+    // if the school does not include Seminary San Diego.
+    // if it is on campus
+    // if the date is after today
+    if( (sizeof($schools) == 0 || !in_array("Seminary San Diego", $schools)) && $offCampusLocation == '' && Date($firstDate) >= time()){
+        $twig = makeTwigEnviron('/code/events/twig');
+        $html = $twig->render('event-alert.html', array());
+
+        echo $html;
+    }
+}
