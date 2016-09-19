278a0bef273f01b2de5cb6546a28aa3f2da453d6
c-schwarze <ces55739@bethel.edu>
2016-09-19 17:23:26 -0400

    quick push for staging only (no good access for xp testing atm)

--- faculty-bios/php/faculty-bio-listing.php
+++ faculty-bios/php/faculty-bio-listing.php
@@ -207,10 +207,15 @@ function get_matched_job_titles_for_bio($bio, $school, $cas, $caps, $gs, $sem) {
         // if school matches
         // A hack for Diane Dahl to appear on any page that includes 'nurs' in the program name
         if( $bio['id'] == 'aab255628c5865131315e7c4685d543b' ) {
+            print_r($school);
+            print_r($cas);
+            print_r($caps);
+            print_r($gs);
             if( ($school == 'College of Arts & Sciences' && check_substring_in_array('nurs', $cas))
                 || ($school == 'College of Adult & Professional Studies' && check_substring_in_array('nurs', $caps))
                 || ($school == 'Graduate School' && check_substring_in_array('nurs', $gs) )) {
                 array_push($matched_job_titles, $display_job_title);
+                break;
             }
         } else {
             if( str_replace('&', 'and', $school) == $job_title['school'] ) {
