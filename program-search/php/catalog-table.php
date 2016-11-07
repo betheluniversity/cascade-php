80156af259d862f0b0e809cfde219a0c5e0a43a6
Philip Gibbens <phg49389@bethel.edu>
2016-11-07 09:55:02 -0600

    Changed catalog table over to a twig file

--- program-search/php/catalog-table.php
+++ program-search/php/catalog-table.php
@@ -1,5 +1,6 @@
 <?php
 
+require $_SERVER["DOCUMENT_ROOT"] . '/code/vendor/autoload.php';
 include_once $_SERVER["DOCUMENT_ROOT"] . "/code/program-search/php/program-search-functions.php";
 
 function get_catalog_table($path){
@@ -9,13 +10,12 @@ function get_catalog_table($path){
         foreach($value["concentrations"] as $L2_Key => $L2_Value){
             if( !empty($L2_Value["catalog_url"]) && strlen($L2_Value["catalog_url"]) > 0 ){
                 if( strcmp($path, $L2_Value["concentration_page"]->{"path"}) == 0 ) {
-                    echo "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
-                    culpa qui officia deserunt mollit anim id est laborum.</p>";
                     $destination_url = str_replace("/index.xml", "/#academicplanstext", $L2_Value["catalog_url"]);
-                    echo "<a href=\"" . $destination_url . "\" class=\"btn\">View Schedule</a><br/>";
+                    $twig = makeTwigEnviron('/code/program-search/twig');
+                    $html = $twig->render('catalog-table.html', array(
+                        'destination_url'=> $destination_url
+                    ));
+                    echo $html;
                     // $xml = file_get_contents($L2_Value["catalog_url"]);
                     // $xml_object = new SimpleXMLElement($xml);
                     // echo $xml_object -> text[0];
