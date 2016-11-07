538d73fead2dcb3fa96eb5c4092a747796d37763
Philip Gibbens <phg49389@bethel.edu>
2016-11-07 09:24:35 -0600

    Adding some text changes that Jake requested

--- program-search/php/catalog-table.php
+++ program-search/php/catalog-table.php
@@ -9,8 +9,13 @@ function get_catalog_table($path){
         foreach($value["concentrations"] as $L2_Key => $L2_Value){
             if( !empty($L2_Value["catalog_url"]) && strlen($L2_Value["catalog_url"]) > 0 ){
                 if( strcmp($path, $L2_Value["concentration_page"]->{"path"}) == 0 ) {
+                    echo "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+                    culpa qui officia deserunt mollit anim id est laborum.</p>";
                     $destination_url = str_replace("/index.xml", "/#academicplanstext", $L2_Value["catalog_url"]);
-                    echo "<a href=\"" . $destination_url . "\" class=\"btn\">Link text</a><br/>";
+                    echo "<a href=\"" . $destination_url . "\" class=\"btn\">View Schedule</a><br/>";
                     // $xml = file_get_contents($L2_Value["catalog_url"]);
                     // $xml_object = new SimpleXMLElement($xml);
                     // echo $xml_object -> text[0];
