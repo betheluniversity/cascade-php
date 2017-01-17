db1fb97e5cc364b24c87e6309ac5544aafe4a0b9
Philip Gibbens <phg49389@bethel.edu>
2017-01-17 16:03:34 -0600

    Renamed for loop variables and fixed some logic for catalog plans

--- program-search/php/catalog-table.php
+++ program-search/php/catalog-table.php
@@ -6,11 +6,11 @@ include_once $_SERVER["DOCUMENT_ROOT"] . "/code/program-search/php/program-searc
 function get_catalog_table($path, $page_title){
     $path = "/" . $path;
     $programs_xml = get_program_xml();
-    foreach ($programs_xml as $key => $value){
-        foreach($value["concentrations"] as $L2_Key => $L2_Value){
-            if( !empty($L2_Value["catalog_url"]) && strlen($L2_Value["catalog_url"]) > 0 ){
-                if( strcmp($path, $L2_Value["concentration_page"]->{"path"}) == 0 ) {
-                    $destination_url = str_replace("/index.xml", "/#academicplanstext", $L2_Value["catalog_url"]);
+    foreach ($programs_xml as $index => $program){
+        foreach($program["concentrations"] as $L2_index => $concentration){
+            if( strlen($concentration["catalog_url"]) > 0 ){
+                if( strcmp($path, $concentration["concentration_page"]->{"path"}) == 0 ){
+                    $destination_url = str_replace("/index.xml", "/#academicplanstext", $concentration["catalog_url"]);
                     $twig = makeTwigEnviron('/code/program-search/twig');
                     $html = $twig->render('catalog-table.html', array(
                         'destination_url'=> $destination_url,
