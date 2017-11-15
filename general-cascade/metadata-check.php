34f965d313d80d48f258346a1e9617263c57262d
Eric Jameson <ericjamesjameson@gmail.com>
2017-11-14 22:22:37 -0600

    check for google, yahoo, bing, before creating cookies for ads

--- general-cascade/metadata-check.php
+++ general-cascade/metadata-check.php
@@ -49,31 +49,36 @@ if (!stristr($host, "bethel.edu") && $referer != null){
 }
 echo "<!-- " . $_SESSION['interesting_referer'] . " -->";
 
+
+// create cookies from utm_ parameters. expire in a year
+$expire = time() + 31536000;
+
+// Set cookie for google/yahoo/bing searches. Check these before proper utm_ get params so the ad
+// data doesn't get overwritten.
+$url = $_SERVER['HTTP_REFERER'];
+$query = parse_url($url, PHP_URL_QUERY);
+$host = parse_url($url, PHP_URL_HOST);
+if( strstr($query,'q=')){
+    if( strstr($host, 'google.')) {
+        setcookie('utm_source', 'google', $expire, "/", ".bethel.edu");
+        setcookie('utm_medium', 'organic', $expire, "/", ".bethel.edu");
+    }
+    elseif( strstr($host, 'yahoo.')) {
+        setcookie('utm_source', 'yahoo', $expire, "/", ".bethel.edu");
+        setcookie('utm_medium', 'organic', $expire, "/", ".bethel.edu");
+    }
+    elseif( strstr($host, 'bing.')) {
+        setcookie('utm_source', 'bing', $expire, "/", ".bethel.edu");
+        setcookie('utm_medium', 'organic', $expire, "/", ".bethel.edu");
+    }
+}
+
 // testing ads:
 // https://www.bethel.edu/graduate/academics/mba/?utm_source=adroll&utm_medium=retargeting&utm_content=mba&utm_campaign=f18_bethel_capsgs_haworth
 foreach( $_GET as $key => $value){
     // if the GET key matches utm_, then add it to the session.
     if( strpos($key, 'utm_') == 0  ){
         // save the cookie value for 4 months
-        setcookie($key, $value, time() + (86400 * 30 * 4), "/", ".bethel.edu");
-    }
-}
-
-// Set cookie for google/yahoo/bing searches
-$url = $_SERVER['HTTP_REFERER'];
-$query = parse_url($url, PHP_URL_QUERY);
-$host = parse_url($url, PHP_URL_HOST);
-if( strstr($query,'q=')){
-    if( strstr($host, 'google.')) {
-        setcookie('utm_source', 'google', time() + (86400 * 30 * 4), "/", ".bethel.edu");
-        setcookie('utm_medium', 'organic', time() + (86400 * 30 * 4), "/", ".bethel.edu");
-    }
-    elseif( strstr($host, 'yahoo.')) {
-        setcookie('utm_source', 'yahoo', time() + (86400 * 30 * 4), "/", ".bethel.edu");
-        setcookie('utm_medium', 'organic', time() + (86400 * 30 * 4), "/", ".bethel.edu");
-    }
-    elseif( strstr($host, 'bing.')) {
-        setcookie('utm_source', 'bing', time() + (86400 * 30 * 4), "/", ".bethel.edu");
-        setcookie('utm_medium', 'organic', time() + (86400 * 30 * 4), "/", ".bethel.edu");
+        setcookie($key, $value, $expire, "/", ".bethel.edu");
     }
 }
