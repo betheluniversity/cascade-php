a6cfed8abd9affea3793779abc47df0f0af3d3e4
c-schwarze <ces55739@bethel.edu>
2018-06-07 13:48:45 -0500

    update event alert construction snippet

--- events/php/event-alert.php
+++ events/php/event-alert.php
@@ -8,17 +8,18 @@
 
 // This method is currently deactivated. Keep it around for the next time we need an alert.
 function create_event_alert($schools, $offCampusLocation, $firstDate){
-//    // convert cascade timestamp to a normal timestamp, and then to a date
-//    $firstDate = Date((int)$firstDate/1000);
-//    $today = time();
-//
-//    // if the school does not include Seminary San Diego.
-//    // if it is on campus
-//    // if the date is after today
-//    if( (sizeof($schools) == 0 || !in_array("Seminary San Diego", $schools)) && $offCampusLocation == '' && Date($firstDate) >= time()){
-//        $twig = makeTwigEnviron('/code/events/twig');
-//        $html = $twig->render('event-alert.html', array());
-//
-//        echo $html;
-//    }
+    // convert cascade timestamp to a normal timestamp, and then to a date
+    $firstDate = Date((int)$firstDate/1000);
+    $today = time();
+
+    // if the school does not include Seminary San Diego.
+    // if it is on campus
+    // if the date is after today
+    // if the date is before august 20
+    if( (sizeof($schools) == 0 || !in_array("Seminary San Diego", $schools)) && $offCampusLocation == '' && Date($firstDate) >= time() && Date($firstDate) <= strtotime("2018-08-20")){
+        $twig = makeTwigEnviron('/code/events/twig');
+        $html = $twig->render('event-alert.html', array());
+
+        echo $html;
+    }
 }
