7d8ed40a028545aecec0457d02c6c8c9f40105d0
c-schwarze <ces55739@bethel.edu>
2018-11-19 17:18:54 -0600

    Add some error handling and result checking

--- banner-blocks/capsgs_checklist.php
+++ banner-blocks/capsgs_checklist.php
@@ -16,8 +16,13 @@ function howToApply($code, $option){
         if( $call_program_code && $option ) {
             $url = "https://wsapi.bethel.edu/capsgs-checklist/$call_program_code/$option";
             echo "<!-- $url -->";
-            $results = json_decode(file_get_contents($url));
-            if( property_exists($results, 'data')){
+            try{
+                $results = json_decode(@file_get_contents($url));
+            } catch(ErrorException $e) {
+                $results = null;
+            }
+
+            if( $results && property_exists($results, 'data')){
                 $results = explode("\n", $results->data);
                 if ($results[0]) {
                     echo $twig->render('capsgs_checklist.html', array(
