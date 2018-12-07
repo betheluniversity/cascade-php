f8a6371287a45fbfde2f61b89510589fde1a1b1f
c-schwarze <ces55739@bethel.edu>
2018-12-07 14:46:53 -0600

    moved test farther up

--- faculty-bios/php/faculty-bio-listing.php
+++ faculty-bios/php/faculty-bio-listing.php
@@ -324,10 +324,10 @@ function sort_bios_by_lead_and_last_name($bios, $top_lead_sort){
 
 // format array to comma separated list with 'and' before the last element
 function format_job_titles($job_titles){
+    return 'TEST';
+
     // remove duplicates
     $job_titles = array_unique($job_titles);
-
-    return 'TEST';
 
     // code from -- http://stackoverflow.com/questions/8586141/implode-array-with-and-add-and-before-last-item
     return join(' and ', array_filter(array_merge(array(join(', ', array_slice($job_titles, 0, -1))), array_slice($job_titles, -1)), 'strlen'));
