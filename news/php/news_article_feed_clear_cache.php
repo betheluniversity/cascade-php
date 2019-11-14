35bf6db256b447e23aac4192209d069ec1a83621
Caleb Schwarze <c-schwarze@bethel.edu>
2019-11-14 14:14:14 -0600

    test

--- news/php/news_article_feed_clear_cache.php
+++ news/php/news_article_feed_clear_cache.php
@@ -20,11 +20,12 @@ $bethel_alert_cache_name = 'clear_cache_bethel_alert_keys';
 $cache->set($bethel_alert_cache_name, 'BLAH', MEMCACHE_COMPRESSED, 300);
 
 $cache_keys = $cache->get($bethel_alert_cache_name);
+$cache_keys_array = explode(':', $cache_keys);
 echo "Cache Keys: $cache_keys";
 
-if( $cache_keys ){
+if( sizeof($cache_keys) > 0 ){
     echo 'HERE WE GO';
-    foreach($cache_keys.explode(':') as $cache_key){
+    foreach($cache_keys_array as $cache_key){
         echo "Cache Key: $cache_key";
         $cache->delete($cache_key);
     }
