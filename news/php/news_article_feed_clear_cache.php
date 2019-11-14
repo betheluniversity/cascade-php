d1cb83e67a59b55625031c18d2a15ff4e7d84c77
Caleb Schwarze <c-schwarze@bethel.edu>
2019-11-14 15:37:42 -0600

    remove the last of logging

--- news/php/news_article_feed_clear_cache.php
+++ news/php/news_article_feed_clear_cache.php
@@ -19,18 +19,13 @@ $bethel_alert_cache_name = 'clear_cache_bethel_alert_keys';
 
 $cache_keys = $cache->get($bethel_alert_cache_name);
 $cache_keys_array = explode(':', $cache_keys);
-print_r("Cache Keys: $cache_keys");
 
 if( $cache_keys ){
-    echo 'HERE WE GO';
     foreach($cache_keys_array as $cache_key){
-        echo "Cache Key: $cache_key";
         $cache->delete($cache_key);
     }
     $cache->delete($bethel_alert_cache_name);
-    echo 'Cleared Cache!';
 }
-
 
 ?>
 
