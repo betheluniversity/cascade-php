04343a3acb0ff6b7878f8389e56671092cdd9881
Caleb Schwarze <c-schwarze@bethel.edu>
2019-12-02 16:00:19 -0600

    update the class in the right spot

--- news/php/news_article_gallery_feed.php
+++ news/php/news_article_gallery_feed.php
@@ -58,8 +58,11 @@ function create_news_article_gallery_feed($categories, $galleryStyle, $myBethel,
 
         // Check if the what the feed type is the same as the article type
         if( ($includeNews && $newsAndStory['article-type'] == 'News') || ($includeStories && $newsAndStory['article-type'] == 'Story') ) {
+            $add_mybethel_class = '';
+            if( strpos($_SERVER['REQUEST_URI'], '_portal/') !== false )
+                $add_mybethel_class = 'img-fluid';
             // Add the srcset to the gallery-image to be passed along to the html twig file
-            $newsAndStory['gallery-image'] = srcset($newsAndStory['image-path'], false, true, '', $newsAndStory['title']);
+            $newsAndStory['gallery-image'] = srcset($newsAndStory['image-path'], false, true, $add_mybethel_class, $newsAndStory['title']);
 
             // don't use this story on this page again
             array_push($GLOBALS['stories-already-used'], $id);
