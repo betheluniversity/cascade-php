f9d6ee19941c5bedcdc7fa3a34c6289af3ba2307
Caleb Schwarze <c-schwarze@bethel.edu>
2020-03-25 16:46:50 -0500

    add in first test

--- general-cascade/global-alert.php
+++ general-cascade/global-alert.php
@@ -1,1 +1,7 @@
 <?php
+
+$alert_file_path = $_SERVER["DOCUMENT_ROOT"] . "/_shared-content/www-mybethel-alerts/www-mybethel-alert.php";
+if( file_exists($alert_file_path) ){
+    $page = file_get_contents($alert_file_path);
+    echo $page;
+}
