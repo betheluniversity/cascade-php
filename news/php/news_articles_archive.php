c8b8034dbf8953fb25ff5c729eac1fe368e497ab
Caleb Schwarze <c-schwarze@bethel.edu>
2020-05-14 11:48:08 -0500

    test

--- news/php/news_articles_archive.php
+++ news/php/news_articles_archive.php
@@ -139,6 +139,7 @@ function inspect_news_archive_page($xml, $categories){
             $page_info['bethel-alert'] = $ds->{'story-metadata'}->{'bethel-alert'};
         }
     } else {
+        print_r('got here, its a problem!');
         # if its not the president announcements page, we want to make sure that the prez announcements are NOT shown.
         foreach ($xml->{'dynamic-metadata'} as $md) {
             $name = $md->name;
