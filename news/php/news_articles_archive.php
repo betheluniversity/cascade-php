06f83a5ee5a703b64e90fb4dab1254618adb66f7
Caleb Schwarze <c-schwarze@bethel.edu>
2020-05-14 11:55:21 -0500

    test

--- news/php/news_articles_archive.php
+++ news/php/news_articles_archive.php
@@ -35,8 +35,9 @@ function create_news_article_archive($categories, $blerts="No"){
         // if we include public alerts, then we only want to skip internal ones
         // if we don't want blerts, then we skip all blerts
         // if we want to include internal, then we don't skip any
-        print($article);
-        print($article['bethel-alert']);
+        print_r('NEXT');
+        print_r($article);
+        print_r($article['bethel-alert']);
         if (($blerts == 'Yes - Public Bethel Alert' and $article['bethel-alert'] == 'Internal Bethel Alert')
             or ($blerts == 'No' and $article['bethel-alert'] != 'No')) {
             continue;
