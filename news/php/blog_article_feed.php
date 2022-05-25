46d099d4e10310e1d1cec48501dc05fc1b5d63df
ac-he <ach28574@bethel.edu>
2022-05-25 14:33:59 -0500

    Update blog_article_feed.php

--- news/php/blog_article_feed.php
+++ news/php/blog_article_feed.php
@@ -36,12 +36,8 @@ function create_news_article_feed_logic($categories, $blerts){
     }
 
     // this is legacy code. It will be used for the archive and for any feed that includes old articles
-    $arrayOfArticles = get_xml($_SERVER["DOCUMENT_ROOT"] . "/_shared-content/xml/articles.xml", $categories, "inspect_news_article");
+    $arrayOfArticles = get_xml($_SERVER["DOCUMENT_ROOT"] . "/testing/anna-h/blog/_feeds/blog-articles-xml", $categories, "inspect_news_article");
 
-    // This is the new version of news.
-    $arrayOfNewsAndStories = get_xml($_SERVER["DOCUMENT_ROOT"] . "/_shared-content/xml/news-and-stories.xml", $categories, "inspect_news_article");
-
-    $arrayOfArticles = array_merge($arrayOfArticles, $arrayOfNewsAndStories);
     global $NumArticles;
     // echo 'feed_news_sorted_'.$NumArticles;
     $sortedArticles = sort_by_date($arrayOfArticles);
