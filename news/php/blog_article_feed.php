699d4d09a07e02c7341988c9b832b323e1961d96
ac-he <ach28574@bethel.edu>
2022-05-25 15:38:38 -0500

    Update blog_article_feed.php

--- news/php/blog_article_feed.php
+++ news/php/blog_article_feed.php
@@ -21,8 +21,9 @@ $DisplayImages;
 $featuredArticleOptions;
 
 //TODO Move to feed_helper later (ACH)
-function get_blog_rss_xml($fileToLoad, $categories, $func){
-    $xml = simplexml_load_file($fileToLoad);
+function get_blog_rss_xml($fileToLoad){
+    $feed = file_get_contents($fileToLoad);
+    $xml = simplexml_load_string($feed);
     $pages = array();
     //$pages = traverse_folder($xml, $pages, $categories, $func);
     return $pages;
@@ -43,7 +44,7 @@ function create_news_article_feed_logic($categories, $blerts){
         $GLOBALS['stories-already-used'] = array();
     }
 
-    $arrayOfArticles = get_blog_rss_xml($_SERVER["DOCUMENT_ROOT"] . "/_testing/anna-h/blog/_feeds/blog-articles-xml.xml", $categories, "inspect_news_article");
+    $arrayOfArticles = get_blog_rss_xml($_SERVER["DOCUMENT_ROOT"] . "/_testing/anna-h/blog/_feeds/blog-articles-xml.xml");
 
     global $NumArticles;
     // echo 'feed_news_sorted_'.$NumArticles;
