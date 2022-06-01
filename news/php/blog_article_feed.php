89198680cdd78bfcfe6ad245aaec3032369182e0
ac-he <ach28574@bethel.edu>
2022-06-01 15:24:05 -0500

    Update blog_article_feed.php

--- news/php/blog_article_feed.php
+++ news/php/blog_article_feed.php
@@ -95,9 +95,10 @@ function get_only_desired_elements($xml)
 
                 echo 'o*0~~~~~~~~~~~~0*o</br>';
                 echo 'res: ' . $res->root.getName . '</br>';
-                foreach ($res->root->children() as $aaa){
-                    echo '~~~'. $aaa->getName() . '</br>';
-                }
+                print_r($res);
+//                foreach ($res->root->children() as $aaa){
+//                    echo '~~~'. $aaa->getName() . '</br>';
+//                }
 //                echo '</br></br>' . $item->title . '</br>';
 //                var_dump($attribAr);
 //                echo '</br>';
@@ -117,7 +118,7 @@ function get_only_desired_elements($xml)
 
 function create_blog_feed()
 {
-    echo "NEW SANITY CHECK: WORKS AS OF JUNE 1 3:21</br></br>";
+    echo "NEW SANITY CHECK: WORKS AS OF JUNE 1 3:23</br></br>";
 
     $feed = file_get_contents($_SERVER["DOCUMENT_ROOT"] . "/_testing/anna-h/blog/_feeds/blog-articles-xml.xml");
     $xml = simplexml_load_string($feed);
