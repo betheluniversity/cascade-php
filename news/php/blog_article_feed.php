26035f73c824ed47f10deedfe74d4203f0ba1f3d
ac-he <ach28574@bethel.edu>
2022-06-02 09:30:37 -0500

    Update blog_article_feed.php

--- news/php/blog_article_feed.php
+++ news/php/blog_article_feed.php
@@ -128,13 +128,13 @@ function get_only_desired_elements($xml)
 
 function create_blog_feed()
 {
-    echo "CURRENT AS OF JUNE 2 9:27</br></br>";
+    echo "CURRENT AS OF JUNE 2 9:30</br></br>";
 
     $feed = file_get_contents($_SERVER["DOCUMENT_ROOT"] . "/_testing/anna-h/blog/_feeds/blog-articles-xml.xml");
     $xml = simplexml_load_string($feed);
 
-    $temp = $xml->getNamespaces();
-    echo $temp;
+    $temp = $xml->getDocNamespaces(TRUE);
+    var_dump($xml);
     //print_s($temp);
 
     $retArray = get_only_desired_elements($xml);
