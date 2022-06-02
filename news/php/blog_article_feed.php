1463d81f576c8ed5445d85496ef927d8445f884e
ac-he <ach28574@bethel.edu>
2022-06-02 15:40:15 -0500

    Update blog_article_feed.php

--- news/php/blog_article_feed.php
+++ news/php/blog_article_feed.php
@@ -133,9 +133,9 @@ function get_only_desired_elements($xml)
                     $retArray[$numItems]['creator'] = (string)$dcNamespace->creator[0];
                 }
                 if ($metadata['categories']){
-                    $retArray[$numItems]['categories'] = "";
+                    $retArray[$numItems]['categories'] = " ";
                     foreach($item->category as $cat){
-                        $retArray[$numItems]['categories'] = $retArray[$numItems]['categories'] . $cat . ", ";
+                        $retArray[$numItems]['categories'] = "{$retArray[$numItems]['categories']} $cat,".PHP_EOL;
 
                     }
                     echo(retArray[$numItems]['categories']);
