2f959b8943b023169c3e37150674cb6d839ecab0
ac-he <ach28574@bethel.edu>
2022-06-02 09:05:42 -0500

    Update blog_article_feed.php

--- news/php/blog_article_feed.php
+++ news/php/blog_article_feed.php
@@ -75,8 +75,8 @@ function get_description_as_array($item)
 
 function get_as_array($item)
 {
-    $descToString = "<root>$item</root>".PHP_EOL;
-    $stringToObj = simplexml_load_string($descToString);
+    //$descToString = "<root>$item</root>".PHP_EOL;
+    $stringToObj = simplexml_load_string($item);
     $objToJson = json_encode($stringToObj);
     $jsonToArr = json_decode($objToJson, TRUE);
 
@@ -122,7 +122,7 @@ function get_only_desired_elements($xml)
 
 function create_blog_feed()
 {
-    echo "NEW SANITY CHECK: WORKS AS OF JUNE 2 9:03</br></br>";
+    echo "NEW SANITY CHECK: WORKS AS OF JUNE 2 9:05</br></br>";
 
     $feed = file_get_contents($_SERVER["DOCUMENT_ROOT"] . "/_testing/anna-h/blog/_feeds/blog-articles-xml.xml");
     $xml = simplexml_load_string($feed);
