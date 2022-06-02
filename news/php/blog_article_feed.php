5a67f39bf6dcb6b0e1e61a6ad08016179307eb5c
ac-he <ach28574@bethel.edu>
2022-06-02 10:52:01 -0500

    Update blog_article_feed.php

--- news/php/blog_article_feed.php
+++ news/php/blog_article_feed.php
@@ -135,7 +135,7 @@ function get_only_desired_elements($xml)
 function create_blog_feed()
 {
     global $allNamespaces;
-    echo "CURRENT AS OF JUNE 2 10:48</br></br>";
+    echo "CURRENT AS OF JUNE 2 10:51</br></br>";
 
     $feed = file_get_contents($_SERVER["DOCUMENT_ROOT"] . "/_testing/anna-h/blog/_feeds/blog-articles-xml.xml");
     $xml = simplexml_load_string($feed);
@@ -159,12 +159,14 @@ function create_blog_feed()
 //    echo "</br>ns</br>";
 //    var_dump($xml->getNamespaces(TRUE));
 //    echo "</br>channel ns</br>";
-//    $chan_ns = $xml->channel->getNamespaces(TRUE);
-//    var_dump($chan_ns);
-//    $chan_ar = $xml->channel->children($chan_ns['sy']);
-//    var_dump($chan_ar);
+    $chan_ns = $xml->channel->getNamespaces(TRUE);
+    var_dump($chan_ns);
+    $chan_ar = $xml->channel->children($chan_ns['sy']);
+    var_dump($chan_ar);
+    $up = $chan_ar['updatePeriod'];
+    var_dump($up);
 //
-//    echo "</br>item ns</br>";
+    echo "</br></br>item ns</br>";
 //    var_dump($xml->channel->item[0]->getNamespaces(TRUE));
 
 
