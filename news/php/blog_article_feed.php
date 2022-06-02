84af83e99330439cfad12cefa7c9146f67bf1e1d
ac-he <ach28574@bethel.edu>
2022-06-02 12:31:48 -0500

    Update blog_article_feed.php

--- news/php/blog_article_feed.php
+++ news/php/blog_article_feed.php
@@ -56,6 +56,9 @@ function set_categories_cats($academics, $admissions, $col_exploration, $col_lif
     setup_individual_category($study, "Study Skills");
     setup_individual_category($wellbeing, "Wellbeing");
     setup_individual_category($all, "All");
+
+    global $categories;
+    dump_var($categories);
 }
 
 function post_matches_cats($post){
@@ -134,7 +137,7 @@ function get_only_desired_elements($xml)
 function create_blog_feed()
 {
     global $allNamespaces;
-    echo "CURRENT AS OF JUNE 2 12:25</br></br>";
+    echo "CURRENT AS OF JUNE 2 12:31</br></br>";
 
     $feed = file_get_contents($_SERVER["DOCUMENT_ROOT"] . "/_testing/anna-h/blog/_feeds/blog-articles-xml.xml");
     $xml = simplexml_load_string($feed);
