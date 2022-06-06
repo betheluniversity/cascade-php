20738b922664153da49a0bf4166eb703fa8fee11
ac-he <ach28574@bethel.edu>
2022-06-06 15:43:10 -0500

    Update blog_article_feed.php

--- news/php/blog_article_feed.php
+++ news/php/blog_article_feed.php
@@ -271,7 +271,6 @@ function get_post_html( $post){
 // Returns an html representation of the heading, to be called by the Velocity code.
 function display_heading(){
     global $completeTitle;
-    echo $completeTitle;
     return "<h2>" . $completeTitle . "</h2>";
 }
 
