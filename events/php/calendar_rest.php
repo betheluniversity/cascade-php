c9f77b4171865135a2e1a1c56d8cb272ffe3f7ef
ac-he <ach28574@bethel.edu>
2022-06-22 11:17:34 -0500

    Update calendar_rest.php

--- events/php/calendar_rest.php
+++ events/php/calendar_rest.php
@@ -225,10 +225,10 @@ function add_event_to_array($dates, $page_data){
 
 function add_page_to_day(&$day, $page){
     if (isset($day)) {
-        $pathToMatch = $page['path'];
         foreach($day as $oldPage){
-            if($oldPage['path'] == $pathToMatch) {
-                // pass
+            if($oldPage['path'] == $page['path']) {
+                $page['title'] = "no";
+                $day[] = $page;
             } else {
                 return;
             }
