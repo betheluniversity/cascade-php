1bae78479bcb8de1fb62aacd3e1100483cc7ffc3
ac-he <ach28574@bethel.edu>
2022-06-22 15:31:04 -0500

    Update calendar_rest.php

--- events/php/calendar_rest.php
+++ events/php/calendar_rest.php
@@ -241,7 +241,8 @@ function add_page_to_day(&$day, &$page, &$dayPaths)
             $day[] = $page;
             $dayPaths[] = $page["path"];
         } else {
-            $day[$findPathKey]['time_string'] = "{$day[$findPathKey]['time_string']}, {$day['time_string']}".PHP_EOL;
+            $day[$findPathKey]['time_string'] = "{$day[$findPathKey]['time_string']}, {$day['time_string']}, {$day['time-string']}".PHP_EOL;
+
         }
     } else {
         $day = array($page);
