7056875f390f995b32ffef13da15b981936bec19
Jacob Jensen <jacob-jensen@bethel.edu>
2022-10-04 16:25:09 -0500

    initial commit

---
+++ salesforce/php/program_licensure.php
@@ -0,0 +1,23 @@
+<?php
+
+function programLicensure($code){
+    $twig = makeTwigEnviron('/code/salesforce/twig');
+    error_log($code, 0);
+    $call_program_code = str_replace("%", "%25", $code);
+    error_log($call_program_code, 0);
+
+    $url = "https://wsapi.xp.bethel.edu/salesforce/state-licensures/$call_program_code";
+
+    echo "<!-- $url -->";
+
+    try {
+        $results = json_decode(@file_get_contents($url));
+    } catch(ErrorException $e) {
+        $results = null;
+    }
+
+    if ($results) {
+        echo $twig->render('program_licensure.html', array(
+            'results' => $results));
+    }
+}
