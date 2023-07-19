99092a8034c0e8b47f13780a63bff1e4de5ec9e4
josiahtillman <jot43536@bethel.edu>
2023-07-19 11:32:27 -0500

    Initial test

--- moodle-enrichment/php/onramps_feed.php
+++ moodle-enrichment/php/onramps_feed.php
@@ -1,6 +1,18 @@
 <?php
 
+function create_moodle_feed(){
+    $feed = autoCache("create_moodle_feed_logic", array(), 300, "No");
+    return $feed;
+}
 
+function create_moodle_feed_logic($categories, $blerts){
+    //include_once $_SERVER["DOCUMENT_ROOT"] . "/code/php_helper_for_cascade.php";
+    //include_once $_SERVER["DOCUMENT_ROOT"] . "/code/general-cascade/feed_helper.php";
 
+    $url = 'https://bethel-university--full.sandbox.my.salesforce.com/services/apexrest/course';
+    $xml = simplexml_load_file($url);
+    print_r($xml);
+    return '1';
+}
 
 ?>
