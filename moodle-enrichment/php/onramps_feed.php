bcaf88208ac4a89825bff84a5e3a78e4d96129f9
josiahtillman <jot43536@bethel.edu>
2023-07-20 10:48:49 -0500

    parsing out data

--- moodle-enrichment/php/onramps_feed.php
+++ moodle-enrichment/php/onramps_feed.php
@@ -8,24 +8,31 @@ function create_moodle_feed(){
 function create_moodle_feed_logic(){
     //include_once $_SERVER["DOCUMENT_ROOT"] . "/code/php_helper_for_cascade.php";
     //include_once $_SERVER["DOCUMENT_ROOT"] . "/code/general-cascade/feed_helper.php";
-
+    $twig = makeTwigEnviron('/code/moodle-enrichment/twig');
     $url = "https://wsapi.xp.bethel.edu/salesforce/moodle-enrichment";
+
     try {
         $results = json_decode(@file_get_contents($url));
     } catch(ErrorException $e) {
         $results = $e;
     }
+
+    $courses = Array();
     foreach ($results as $class) {
         $class_array = json_decode(json_encode($class), true);
-        print "Name: " . $class_array[Name] . "\n";
-        print "Description: " . $class_array[Description__c] . "\n";
-        print "ID: " . $class_array[Id] . "\n";
-        print "Registration Cost: " . $class_array[Registration_Cost__c] . "\n";
-        print "Attributes: " . $class_array[attributes] . "\n";
-        print "Start Date: " . $class_array[hed__Start_Date__c] . "\n";
-        print "End Date: " . $class_array[hed__End_Date__c] . "\n";
+        print "Name: " . $class_array[Name] . "</br>";
+        print "Description: " . $class_array[Description__c] . "</br>";
+        print "ID: " . $class_array[Id] . "</br>";
+        print "Registration Cost: " . $class_array[Registration_Cost__c] . "</br>";
+        print "Attributes: " . $class_array[attributes] . "</br>";
+        print "Start Date: " . $class_array[hed__Start_Date__c] . "</br>";
+        print "End Date: " . $class_array[hed__End_Date__c] . "</br>";
+//        $html = $twig->render('onramps_feed.html', array(
+//            'results' => $course));
     }
-    print_r($class_array);
+
+
+
     return '1';
 }
 
