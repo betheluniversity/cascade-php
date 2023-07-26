9bf19a15689f4b9dc111c887847622475999ce97
Jacob Jensen <jacob-jensen@bethel.edu>
2023-07-26 09:10:13 -0500

    add course offerings parameter

---
+++ salesforce/php/course_offerings.php
@@ -0,0 +1,39 @@
+<?php
+
+function create_moodle_feed($org_id){
+    $feed = autoCache("create_moodle_feed_logic", array($org_id), 300, "No");
+    return $feed;
+}
+
+function create_moodle_feed_logic($org_id){
+
+    $twig = makeTwigEnviron('/code/salesforce/twig');
+
+    $staging = strstr(getcwd(), "/staging");
+
+    //Changes the authenticating URL depending on the staging environment
+    if ($staging){
+        $wsapi_url = 'https://wsapi.xp.bethel.edu/salesforce/moodle-enrichment';
+    }else{
+        $wsapi_url = 'https://wsapi.bethel.edu/salesforce/moodle-enrichment';
+    }
+
+    if ($org_id) {
+        $wsapi_url .= "/";
+        $wsapi_url .= $org_id;
+    }
+
+    try {
+        $results = json_decode(@file_get_contents($wsapi_url));
+    } catch(ErrorException $e) {
+        $results = $e;
+    }
+
+    $courses = Array();
+    foreach ($results as $class) {
+        array_push( $courses, json_decode(json_encode($class), true));
+    }
+
+    return $twig->render('course_offerings.html', array('courses' => $courses));
+}
+
