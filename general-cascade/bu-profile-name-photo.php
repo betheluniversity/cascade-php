233cb58a3e7602468a9650997b6f963d8b92479a
Jacob Jensen <jacob-jensen@bethel.edu>
2024-10-22 17:04:28 -0500

    add function to get name and image from username

---
+++ general-cascade/bu-profile-name-photo.php
@@ -0,0 +1,18 @@
+<?php
+function get_name_and_photo($userName) {
+    // Example data, replace with actual logic to fetch data
+    $data = array(
+        "imageURL" => "https://example.com/image.jpg",
+        "fullName" => $userName
+    );
+    echo json_encode($data);
+}
+
+// Check if userName parameter is set in the query string
+if (isset($_GET['userName'])) {
+    $userName = $_GET['userName'];
+    get_name_and_photo($userName);
+} else {
+    echo json_encode(array("error" => "userName parameter is missing"));
+}
+?>
