59d611990e3a95e7d4092dbef5bc0ff9557d338a
Jacob Jensen <jacob-jensen@bethel.edu>
2024-10-23 11:38:52 -0500

    test

--- general-cascade/bu-profile-name-photo.php
+++ general-cascade/bu-profile-name-photo.php
@@ -1,12 +1,11 @@
 <?php
 function get_name_and_photo($userName) {
     // Example data, replace with actual logic to fetch data
-    //$data = array(
-    //    "imageURL" => "https://example.com/image.jpg",
-    //    "fullName" => $userName
-    //);
-    //echo json_encode($data);
-    return 'test';
+    $data = array(
+        "imageURL" => "https://example.com/image.jpg",
+        "fullName" => $userName
+    );
+    echo json_encode($data);
 }
 
 // Check if userName parameter is set in the query string
