e92d051934642458d1561cbf5668a0bab363d531
johjacb <johjacb@bethel.edu>
2024-11-22 10:36:22 -0600

    ai's try

--- general-cascade/staging-banner.php
+++ general-cascade/staging-banner.php
@@ -1,16 +1,20 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jot43536
- * Date: 6/19/15
- * Time: 2:19 PM
- */
 
-$twig = makeTwigEnviron('/code/general-cascade/twig');
+$live_url = "https://www.bethel.edu"; // Example value
 
-echo $twig->render('staging-banner.html', array(
-    'staging' => $staging,
-    'cms_url' => $cms_url,
-    'page_path' => $_SERVER['REQUEST_URI'],
-    'liveURL' => rtrim("https://www.bethel.edu", '/') . '/' . ltrim($_SERVER['REQUEST_URI'], '/')
-))
+// Check if REQUEST_URI is set
+$requestUri = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '/';
+
+// Render the Twig template
+try {
+    echo $twig->render('staging-banner.html', array(
+        'staging' => $staging,
+        'cms_url' => $cms_url,
+        'page_path' => $requestUri,
+        'liveURL' => rtrim("https://www.bethel.edu", '/') . '/' . ltrim($requestUri, '/')
+    ));
+} catch (Exception $e) {
+    // Handle the error (log it, display a message, etc.)
+    echo 'Error rendering template: ' . $e->getMessage();
+}
+?>
