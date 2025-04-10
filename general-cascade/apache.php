fa68ec8a70aff2cca8ca511dffcd005e24421801
Jacob Jensen <jacob-jensen@bethel.edu>
2025-04-10 16:53:21 -0500

    apache check authentication

--- general-cascade/apache.php
+++ general-cascade/apache.php
@@ -1,12 +1,31 @@
 <?php
 
-$require_auth = 'Yes';
-$auth_type = 'Microsoft';
+session_start();
 
-if (isset($_GET['redirect'])) {
-    $canonical_url = $_GET['redirect'];
+// Check if the user is authenticated
+if (!isset($_SERVER['OIDC_CLAIM_sub'])) {
+
+    // Store the original request URI
+    $request_uri = $_GET['request_uri'];
+    $_SESSION['request_uri'] = $request_uri;
+
+    // Handle Microsoft Auth
+    $require_auth = 'Yes';
+    $auth_type = 'Microsoft';
+    $canonical_url = $request_uri;
     require_once 'auth.php';
+    exit();
 }
-header("Location: $canonical_url");
+
+// User is authenticated, handle the original request
+if (isset($_SESSION['request_uri'])) {
+    $request_uri = $_SESSION['request_uri'];
+    unset($_SESSION['request_uri']);
+    header('Location: ' . $request_uri);
+    exit();
+}
+
+// Default behavior if no original request URI is set
+header('Location: https://www.bethel.edu');
 exit();
 ?>
