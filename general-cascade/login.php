76a3315dca5ae3d3deb953e37b3f82a79ae692c4
johjacb <johjacb@bethel.edu>
2026-08-25 14:22:57 -0500

    attempting auth

--- general-cascade/login.php
+++ general-cascade/login.php
@@ -3,12 +3,12 @@
 // Get the URL that called this script
 $redirect_url = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : "https://$_SERVER[HTTP_HOST]";
 
-// Check if the user is authenticated
-if (!isset($_COOKIE['remote-user'])) {
-    // Handle Auth
-    $require_auth = 'Yes';
-    require_once 'auth.php';
-} else {
-    header("Location: $redirect_url");
-}
+// Always validate the server-side authentication session. The remote-user
+// cookie can outlive that session and must not be used as proof of login.
+$require_auth = 'Yes';
+require_once 'auth.php';
+
+// forceAuthentication() redirects unauthenticated users to Microsoft. If the
+// user is already authenticated, return them to the page that initiated login.
+header("Location: $redirect_url");
 ?>
